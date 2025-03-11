c046b2857877cf5d6a4922d1eacc0cf0560d6ff6
khubaib shah <bewithbaacha@gmail.com>
2025-03-11 21:03:24 +0500

    improve API base URL configuration

--- admin/src/config/config.js
+++ admin/src/config/config.js
@@ -1,8 +1,10 @@
 import axios from "axios";
-
+const URL =
+  import.meta.env.VITE_API_BASE_URL_PRO ||
+  import.meta.env.VITE_API_BASE_URL_DEV;
 // Create an Axios instance with a base URL
 export const baseUri = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL: URL,
   headers: {
     "Content-Type": "application/json",
   },
