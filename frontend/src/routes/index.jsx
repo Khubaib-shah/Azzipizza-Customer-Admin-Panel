17ce91f14f706e052e13ff78af46c43e5e8ede4c
khubaib shah <bewithbaacha@gmail.com>
2025-04-02 19:49:34 +0500

    fix: update import path for Contact page to ContactUs

--- frontend/src/routes/index.jsx
+++ frontend/src/routes/index.jsx
@@ -2,7 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import Home from "../pages/Home";
 import About from "../pages/About";
-import Contact from "../pages/Contact";
+import Contact from "../pages/ContactUs";
 import Cart from "../pages/Cart";
 import OrderHistory from "../pages/OrderHistory";
 // components
