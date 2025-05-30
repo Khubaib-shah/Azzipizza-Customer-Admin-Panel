c2ea68c13d99a6cb0839cac79cd96d3fd176d922
khubaib shah <bewithbaacha@gmail.com>
2025-05-30 13:37:29 +0500

    fix: update API base URL priority to use production environment first

--- frontend/src/config/config.js
+++ frontend/src/config/config.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const URL =
-  import.meta.env.VITE_API_BASE_URL_DEV ||
+  import.meta.env.VITE_API_BASE_URL_PRO ||
   import.meta.env.VITE_API_BASE_URL_DEV;
 
 export const baseUri = axios.create({
