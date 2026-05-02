28b10357ac0b799b610f7c5d55b381933b2ae3f6
Khubaib-shah <bewithbaacha@gmail.com>
2026-05-03 02:01:18 +0500

    Added Audio manager in the admin layout

--- src/apps/admin/components/AdminLayout.jsx
+++ src/apps/admin/components/AdminLayout.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Sidebar from "./Sidebar";
 import Header from "./common/Header";
+import AudioManager from "./AudioManager";
 
 const AdminLayout = () => {
   return (
     <div className="min-h-screen bg-gray-100">
+      <AudioManager />
       <Header />
       <div className="grid grid-cols-1 md:grid-cols-10 lg:grid-cols-12 gap-0">
         <div className="col-span-3 md:col-span-3 lg:col-span-3">
